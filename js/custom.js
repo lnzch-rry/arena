82b5b5ac67335c4b3b2ea22ad42f5ebcf76d4f25
Lindsay Cherry <lnzcherry@gmail.com>
2017-08-17 16:18:43 -0400

    Push files
    push existing file structure to GH pages

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function () {
+
+    // banner
+    const video = document.querySelector("video");
+    if (window.matchMedia('(prefers-reduced-motion)').matches) {
+      video.removeAttribute("autoplay");
+      video.pause();
+    }
+    $('.vid-container').height($('#banner svg').height());
+
+    window.addEventListener('resize', function () {
+        $('.vid-container').height($('#banner svg').height());
+    });
+
+    // banner play video
+    $('button.play').click(function() {
+        $('.vid-container').css('height', '100%');
+    });
+
+    // button hovers
+    $(".single-product .trans-btn").hover(function () {
+        $(this).toggleClass("trans-btn");
+     });
+
+    $(".shop-more").hover(function () {
+        $(this).toggleClass("trans-btn");
+     });
+
+});
