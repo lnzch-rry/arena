11424df3af5594e1f16ee86b59ce7e910f6fdd93
Lindsay Cherry <lnzcherry@gmail.com>
2017-10-16 14:19:06 -0400

    change class names and names of images

--- js/custom.js
+++ js/custom.js
@@ -29,9 +29,14 @@ $(document).ready(function () {
 
     // image/video functionality on hover
     $('.single-product .image').each(function() {
-        let imgClass = $(this).attr('class');
-        console.log(imgClass);
+        let imgClass = $(this).attr('class').split(' ')[1];
+        $(this).css("background-image", "url(../images/" + imgClass + ".jpg)");
 
+        $(this).hover(function() {
+            $(this).css("background-image", "url(../images/" + imgClass + ".gif)");
+            }, function() {
+              $(this).css("background-image", "url(../images/" + imgClass + ".jpg)");
+        });
 
     });
 
